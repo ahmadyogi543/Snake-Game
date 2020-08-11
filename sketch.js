a1aab3d5c9d777fee8b53637be2325d20b80a688
ahmadyogi543 <ahmadyogi543@gmail.com>
2020-08-11 13:31:24 +0800

    Added everything!

--- sketch.js
+++ sketch.js
@@ -1,64 +1,119 @@
 
-//Global variables
+//Variabel global
+const scl = 10;
 let head;
-let scl = 10;
 let border;
+let food;
+let bodies = [];
 
-//setup every object and in game
+//Mensetup game
 function setup(){
 
-	//creating a canvas
+	//membuat kanvas
 	createCanvas(500,500);
 
-	//set a framerate to same as the scale
+	//menset framerate game
 	frameRate(scl);
 
-	//Object creations
+	//kepala ular
 	head = new Snake(width/2,height/2,255);
 
+	//border/batasan
 	border = {
-		x: 0 + 5*scl,
-		y: 0 + 5*scl,
-		size: width - 10*scl,
+
+		// Batas game berada di titik 6*scl yakni 60
+		x: 6*scl,
+		y: 6*scl,
+		size: width - 12*scl,
+
 		show: function(){
 			stroke(255);
-			strokeWeight(3);
+			strokeWeight(1);
 			noFill();
 
 			square(this.x,this.y,this.size);
 		}
 	};
+	
+	//makanan ular
+	food = {
+		x: width/2 + 100,
+		y: height/2,
+		size: scl,
+		show: function(){
+			noStroke();
+			fill(255,0,0);
+			
+			square(this.x,this.y,this.size);
+		},
+
+		newPos: function(){
+			let positions = [];
+			for(let i = 6*scl; i <= width - 7*scl; i += 10){
+				positions.push(i);
+			}
+			this.x = positions[floor(random(positions.length))];
+			this.y = positions[floor(random(positions.length))];
+		}
+	};
 }
 
-//draw every object on canvas
+//Mengambar semua objek di kanvas
 function draw(){
-	background(0);
+	background(51);
 
 	border.show();
+	
+	food.show();
 
 	head.update();
+	
+
+	for(let i = 0; i < bodies.length; i++){
+		bodies[i].update();
+		bodies[i].show();
+	}
+
+	if(head.isEats(food)){
+		food.newPos();
+
+		bodies.push(new Snake(width/2,height/2,200));
+	}
+
+	for(let i = bodies.length - 1; i > 0; i--){
+		bodies[i].x = bodies[i - 1].x;
+		bodies[i].y = bodies[i - 1].y;
+	}
+
+	if(bodies.length > 0){
+		bodies[0].x = head.x;
+		bodies[0].y = head.y;
+	}
+
 	head.show();
+
 }
 
-//function for detect a key if its being pressed by player
+//fungsi untuk mendeteksi apakah user memencet keyboard
 function keyPressed(){
-	//move left
-	if(keyCode == 37){
+
+	//ke kiri
+	if(keyCode === LEFT_ARROW){
 		head.setSpeed(-scl,0);
 	}
 
-	//move up
-	else if(keyCode == 38){
+	//ke atas
+	else if(keyCode == UP_ARROW){
 		head.setSpeed(0,-scl);
 	}
 
-	//move right
-	else if(keyCode == 39){
+	//ke kanan
+	else if(keyCode == RIGHT_ARROW){
 		head.setSpeed(scl,0);
 	}
 
-	//move down
-	else if(keyCode == 40){
+	//ke bawah
+	else if(keyCode == DOWN_ARROW){
 		head.setSpeed(0,scl);
 	}
 }
--- snake.js
+++ snake.js
@@ -1,4 +1,4 @@
-//Function constructor for making snake object
+//Function constructor untuk membuat objek ular
 
 function Snake(x,y,color){
 	this.x = x;
@@ -7,6 +7,7 @@ function Snake(x,y,color){
 	this.dy = 0;
 	this.size = scl;
 	this.color = color;
+	this.hitsBorder = false;
 
 	this.show = function(){
 		fill(this.color);
@@ -17,10 +18,34 @@ function Snake(x,y,color){
 
 	this.update = function(){
 		
-		//Pergerakan ular
+		//Laju ular
 		this.x += this.dx;
 		this.y += this.dy;
 
+		//hits border
+		if(this.x < 6*scl){
+			this.x = width - 7*scl;
+		}
+		else if(this.x > width - 7*scl){
+			this.x = 6*scl;
+		}
+		else if(this.y < 6*scl){
+			this.y = height - 7*scl;
+		}
+		else if(this.y > height - 7*scl){
+			this.y = 6*scl;
+		}
+
+	}
+
+	this.isEats = function(food){
+		return this.x === food.x && this.y === food.y;
+	}
+
+	this.resetState = function(){
+		this.x = width/2;
+		this.y = height/2;
+		this.dx = this.dy = 0;
 	}
 
 	this.setSpeed = function(dx,dy){
