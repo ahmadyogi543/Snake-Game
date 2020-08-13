fd51eb39bf20665fc7ca15b22efc2030558173b5
ahmadyogi543 <ahmadyogi543@gmail.com>
2020-08-13 19:04:12 +0800

    Added new feature

--- sketch.js
+++ sketch.js
@@ -1,27 +1,35 @@
 
-//Variabel global
+//Skala
 const scl = 10;
+
+//Kepala ular
 let head;
+
+//Batas
 let border;
+
+//Makanan ular
 let food;
+
+//Badan ular
 let bodies = [];
 
 //Mensetup game
 function setup(){
 
-	//membuat kanvas
+	//Membuat kanvas
 	createCanvas(500,500);
 
-	//menset framerate game
+	//Menset framerate game
 	frameRate(scl);
 
-	//kepala ular
+	//Kepala ular
 	head = new Snake(width/2,height/2,255);
 
-	//border/batasan
+	//Batas
 	border = {
 
-		// Batas game berada di titik 6*scl yakni 60
+		// Batas game berada di titik 6*scl
 		x: 6*scl,
 		y: 6*scl,
 		size: width - 12*scl,
@@ -34,12 +42,23 @@ function setup(){
 			square(this.x,this.y,this.size);
 		}
 	};
-	
-	//makanan ular
+
+	//Posisi makanan ular
+	//Membuat list yang menyimpan posisi posisi yang memungkinkan makanan
+	//Untuk dimunculkan dengan memerhatikan jika makanan muncul di badan ular
+	//Makanan muncul di tempat lain
+	let pos = [];
+	for(let i = 6*scl; i <= width - 7*scl; i += 10){
+		pos.push(i);
+	}
+
+	//Makanan ular
 	food = {
 		x: width/2 + 100,
 		y: height/2,
 		size: scl,
+		positions: pos,
+
 		show: function(){
 			noStroke();
 			fill(255,0,0);
@@ -47,13 +66,13 @@ function setup(){
 			square(this.x,this.y,this.size);
 		},
 
-		newPos: function(){
-			let positions = [];
-			for(let i = 6*scl; i <= width - 7*scl; i += 10){
-				positions.push(i);
+		newPos: function(body){
+			let x = this.positions[floor(random(this.positions.length))];
+			let y = this.positions[floor(random(this.positions.length))];
+
+			if(x != body.x && y != body.y){
+				this.x = x; this.y = y;
 			}
-			this.x = positions[floor(random(positions.length))];
-			this.y = positions[floor(random(positions.length))];
 		}
 	};
 }
@@ -68,30 +87,46 @@ function draw(){
 
 	head.update();
 	
+	//menggambar dan mengupdate badan ular	
+	for(let i = bodies.length - 1; i > -1; i--){
 
-	for(let i = 0; i < bodies.length; i++){
 		bodies[i].update();
 		bodies[i].show();
+
+		if(i != 0){
+			bodies[i].x = bodies[i - 1].x;
+			bodies[i].y = bodies[i - 1].y;
+
+			//mencek apakah kepala ular mengenai badannya
+			if(bodies[i].x === head.x && bodies[i].y === head.y){
+
+				//Menghapus badan ular
+				for(let i = bodies.length - 1; i > -1; i--){
+					bodies.pop();
+				}
+
+				head.resetState();
+				break;
+			}
+		}
 	}
 
+	//mencek apakah kepala ular memakan makanan
 	if(head.isEats(food)){
-		food.newPos();
-
-		bodies.push(new Snake(width/2,height/2,200));
+		for(let i = bodies.length - 1; i > -1; i--){
+			food.newPos(bodies[i]);
+		}
+		bodies.push(new Snake(width,height,180));
 	}
 
-	for(let i = bodies.length - 1; i > 0; i--){
-		bodies[i].x = bodies[i - 1].x;
-		bodies[i].y = bodies[i - 1].y;
-	}
-
+	//mencek apakah panjang ular lebih > 1
+	//dengan kata lain mencek apakah ular memiliki badan atau tidak
 	if(bodies.length > 0){
 		bodies[0].x = head.x;
 		bodies[0].y = head.y;
 	}
 
 	head.show();
-
 }
 
 //fungsi untuk mendeteksi apakah user memencet keyboard
@@ -117,3 +152,4 @@ function keyPressed(){
 		head.setSpeed(0,scl);
 	}
 }
+
--- snake.js
+++ snake.js
@@ -7,8 +7,8 @@ function Snake(x,y,color){
 	this.dy = 0;
 	this.size = scl;
 	this.color = color;
-	this.hitsBorder = false;
 
+	//fungsi untuk menggambar
 	this.show = function(){
 		fill(this.color);
 		noStroke();
@@ -16,13 +16,14 @@ function Snake(x,y,color){
 		square(this.x,this.y,this.size);
 	}
 
+	//mengupdate kondisi ular
 	this.update = function(){
 		
 		//Laju ular
 		this.x += this.dx;
 		this.y += this.dy;
 
-		//hits border
+		//memindahkan ular kesebelah batasnya
 		if(this.x < 6*scl){
 			this.x = width - 7*scl;
 		}
@@ -38,21 +39,25 @@ function Snake(x,y,color){
 
 	}
 
+	//fungsi ular untuk mengetahui apa kepala sdh memakan makanan
 	this.isEats = function(food){
+		//mencek dengan membandingkan posisi kepala dan makanan
 		return this.x === food.x && this.y === food.y;
 	}
 
+	//mereset ular
 	this.resetState = function(){
 		this.x = width/2;
 		this.y = height/2;
 		this.dx = this.dy = 0;
 	}
 
+	//fungsi untuk menset arah pergerakan ular
 	this.setSpeed = function(dx,dy){
 		
 		//Mengatur pergerakan ular dengan memperhatikan value dx dan dy
 		//Jika dx != 0 maka dy = 0 dan jika dy != 0 maka dx = 0
-
+		//Dengan tujuan agar ular tidak bisa bergerak mundur
 		if(this.dx != -dx || this.dy != -dy){
 			this.dx = dx;
 			this.dy = dy;
